2f94e1f5c748897423b227aa0775f0287f8a2c94
Adam Huganir <adam@huganir.com>
2024-02-05 13:03:33 -0500

    added some error handling and some doc

--- git-helpers.sh
+++ git-helpers.sh
@@ -1,16 +1,18 @@
+# This function generates a git tag with a timestamp and the current git commit hash.
+# If there are any uncommitted changes, or any other weird situation, '-dirty' is appended to the tag.
+# the date format can be added as an argument though it defaults to +%Y%m%d-%H%M
 git-tag-dated() {
-    local datefmt
+    local dateformat
     local datestamp
-    local gitref
+    local shortref
     local tag
-    datefmt="${1:-+%Y%m%d-%H%M}"
-    datestamp="$(date "$datefmt")"
-    gitref="$(git rev-parse --short HEAD)"
-    tag="${datestamp}-${gitref}"
-    echo -n "${tag}"
+    dateformat="${1:-+%Y%m%d-%H%M}"
+    datestamp="$(date "$dateformat")"
+    shortref="$(git rev-parse --short HEAD)" || { echo "Failed to get git commit hash, are you in a git repository?"; exit 1; }
+    tag="${datestamp}-${shortref}"
+    # if there are any difference, regardless of what they are, the tag is marked dirty
     if ! [ "$(git status --short)" = "" ]; then
-        echo '-dirty'
-    else
-        echo
+        tag="${tag}-dirty"
     fi
+    echo -n "${tag}"
 }
