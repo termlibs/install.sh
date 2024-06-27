3e3331351f3a940e014f8439016e54d49df3c258
Adam Huganir <adam@huganir.com>
2024-06-27 12:23:29 -0400

    Update git-helpers.sh

--- git-helpers.sh
+++ git-helpers.sh
@@ -1,18 +1,10 @@
-# This function generates a git tag with a timestamp and the current git commit hash.
-# If there are any uncommitted changes, or any other weird situation, '-dirty' is appended to the tag.
+# This function generates a git tag with a timestamp and the current git commit hash and status.
 # the date format can be added as an argument though it defaults to +%Y%m%d-%H%M
-git-tag-dated() {
-    local dateformat
-    local datestamp
-    local shortref
-    local tag
-    dateformat="${1:-+%Y%m%d-%H%M}"
-    datestamp="$(date "$dateformat")"
-    shortref="$(git rev-parse --short HEAD)" || { echo "Failed to get git commit hash, are you in a git repository?"; exit 1; }
-    tag="${datestamp}-${shortref}"
-    # if there are any difference, regardless of what they are, the tag is marked dirty
-    if ! [ "$(git status --short)" = "" ]; then
-        tag="${tag}-dirty"
-    fi
-    echo -n "${tag}"
+git_tag_dated() {
+  local rev rev_string full_rev_sring dateformat
+  dateformat="${1:-+%Y%m%d-%H%M}"
+  rev=$(git rev-parse --short HEAD)
+  full_rev_string="$(git describe --broken --dirty --always)"
+  rev_string="${full_rev_string/*$rev/$rev}" # strip any tags
+  printf "%s-%s" "$(date "$dateformat")" "$rev_string"
 }
